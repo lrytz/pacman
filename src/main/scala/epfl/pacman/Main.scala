000865b5c19df7ddc1b27148a742a3e43b86c5e6
dubochet <dubochet@c2b5fe70-0d1f-0410-848a-a28dd871345c>
2010-05-06 14:09:59 +0000

    Added ignore patterns for SBT (and a first, dummy source file).
    git-svn-id: https://lampsvn.epfl.ch/svn-repos/lamp/pacman/trunk@4547 c2b5fe70-0d1f-0410-848a-a28dd871345c

---
+++ src/main/scala/epfl/pacman/Main.scala
@@ -0,0 +1,15 @@
+package epfl.pacman
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: dubochet
+ * Date: 06-May-2010
+ * Time: 16:06:48
+ * To change this template use File | Settings | File Templates.
+ */
+
+object Main extends Application {
+
+  println("I am PacMan!")
+
+}
