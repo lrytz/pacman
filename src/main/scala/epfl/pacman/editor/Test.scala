35d01f073b9fc0c181e3b24a8a033ea2d37e457c
dubochet <dubochet@c2b5fe70-0d1f-0410-848a-a28dd871345c>
2010-05-27 15:14:34 +0000

    Test file for ScalaPane.
    git-svn-id: https://lampsvn.epfl.ch/svn-repos/lamp/pacman/trunk@4602 c2b5fe70-0d1f-0410-848a-a28dd871345c

---
+++ src/main/scala/epfl/pacman/editor/Test.scala
@@ -0,0 +1,34 @@
+package epfl.pacman.editor
+
+import scala.swing._
+import event.ButtonClicked
+
+object Test extends SimpleSwingApplication {
+
+  def top = new MainFrame {
+    title = "Editor Test"
+    contents = new GridBagPanel {
+      import GridBagPanel._
+      val pane = new ScalaPane {
+        preferredSize = new Dimension(300, 300)
+      }
+      layout(pane) = new Constraints {
+        gridx = 0
+        gridy = 0
+        insets = new Insets(10, 10, 10, 10)
+      }
+      val button = new Button("Test")
+      layout(button) = new Constraints {
+        gridx = 0
+        gridy = 1
+        insets = new Insets(10, 10, 10, 10)
+      }
+      listenTo(button)
+      reactions += {
+        case ButtonClicked(`button`) =>
+          println("Click me baby, one more time")
+      }
+    }
+  }
+
+}
