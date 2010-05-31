a1d8b8041c4e17882c0d67f41d38693b6681e763
rytz <rytz@c2b5fe70-0d1f-0410-848a-a28dd871345c>
2010-05-31 10:27:11 +0000

    proguard plugin for sbt (not working...)
    git-svn-id: https://lampsvn.epfl.ch/svn-repos/lamp/pacman/trunk@4638 c2b5fe70-0d1f-0410-848a-a28dd871345c

--- project/build/Pacman.scala
+++ project/build/Pacman.scala
@@ -1,5 +1,7 @@
 import sbt._
 
-class Pacman(info: ProjectInfo) extends DefaultProject(info) {
+class Pacman(info: ProjectInfo) extends ProguardProject(info) {
   val scalaSwing = "org.scala-lang" % "scala-swing" % "2.8.0.RC2"
+
+  // proguard stuff todo...
 }
