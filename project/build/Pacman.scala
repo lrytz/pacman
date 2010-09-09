282b822833f0862117329565ea81894540db552d
rytz <rytz@c2b5fe70-0d1f-0410-848a-a28dd871345c>
2010-09-09 09:55:53 +0000

    use 2.8.0 final. tried proguard config but doesn't work
    git-svn-id: https://lampsvn.epfl.ch/svn-repos/lamp/pacman/trunk@4647 c2b5fe70-0d1f-0410-848a-a28dd871345c

--- project/build/Pacman.scala
+++ project/build/Pacman.scala
@@ -1,30 +1,22 @@
 import sbt._
 import proguard.{Configuration=>ProGuardConfiguration, ProGuard, ConfigurationParser}
 
-class Pacman(info: ProjectInfo) extends ProguardProject(info) {
-  val scalaSwing = "org.scala-lang" % "scala-swing" % "2.8.0.RC2"
+class Pacman(info: ProjectInfo) extends DefaultProject(info) with ProguardProject {
+  val scalaSwing = "org.scala-lang" % "scala-swing" % "2.8.0"
+
+  override val artifactID = "pacman"
+
+  override def mainClass = Some("epfl.pacman.Main")
 
 
-  // @TODO: doesn't work...
+  override def proguardOptions = List(
+   "-keepclasseswithmembers public class * { public static void main(java.lang.String[]); }",
+    "-dontoptimize",
+    "-dontobfuscate",
+    proguardKeepLimitedSerializability,
+    proguardKeepAllScala,
+    "-keep interface scala.ScalaObject"
+  )
 
-  val args =
-"""-keep public class epfl.pacman.Main {
-    public static void main(java.lang.String[]);
-  }""" ::
-//"-keep class epfl.pacman.**" ::
-"-injars project/boot/scala-2.8.0.RC2/lib/scala-library.jar" ::
-"-injars project/boot/scala-2.8.0.RC2/lib/scala-compiler.jar" ::
-"-injars lib_managed/scala_2.8.0.RC2/compile/scala-swing-2.8.0.RC2.jar" ::
-"-injars target/scala_2.8.0.RC2/pacman_2.8.0.RC2-1.0.jar" ::
-"-dontwarn" :: "-dontoptimize" :: "-dontobfuscate" ::
-"-outjars target/scala_2.8.0.RC2/pacman_2.8.0.RC2-1.0.min.jar" ::
-//"-libraryjars "+System.getProperty("java.home")+"/lib/rt.jar" ::
-Nil
-
-  override def proguardTask = task {
-    val config = new ProGuardConfiguration
-    new ConfigurationParser(args.toArray[String], info.projectPath.asFile).parse(config)
-    new ProGuard(config).execute
-    None
-  }
+  override def proguardInJars = Path.fromFile("lib_managed/scala_2.8.0/compile/scala-swing-2.8.0.jar") +++ Path.fromFile(scalaLibraryJar) +++ super.proguardInJars
 }
