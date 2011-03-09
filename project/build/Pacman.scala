645b3d326a3e939c3535d76e1cff8b1b0906b3fb
Lukas Rytz <lukas.rytz@epfl.ch>
2011-03-09 22:57:40 +0100

    added sbt-proguard to build one big executable jar

--- project/build/Pacman.scala
+++ project/build/Pacman.scala
@@ -2,23 +2,25 @@ import sbt._
 import proguard.{Configuration=>ProGuardConfiguration, ProGuard, ConfigurationParser}
 
 class Pacman(info: ProjectInfo) extends DefaultProject(info) with ProguardProject {
-  val scalaSwing = "org.scala-lang" % "scala-swing" % "2.8.1.RC2"
+  val scalaSwing = "org.scala-lang" % "scala-swing" % "2.8.1"
 
   override val artifactID = "pacman"
 
   override def mainClass = Some("epfl.pacman.Main")
 
 
-  // @TODO: proguard stuff does not work.
+  // @TODO: for now, it just includes everything. also manually specifying jars is ugly.
+  // http://stackoverflow.com/questions/2887655/making-stand-alone-jar-with-simple-build-tool
+
+  override def proguardInJars = super.proguardInJars +++ scalaLibraryPath +++ Path.fromFile("project/boot/scala-2.8.1/lib/scala-compiler.jar") +++ Path.fromFile("lib_managed/scala_2.8.1/compile/scala-swing-2.8.1.jar")
 
   override def proguardOptions = List(
-   "-keepclasseswithmembers public class * { public static void main(java.lang.String[]); }",
+    "-keep class epfl.** { *; }",
+    "-keep class scala.** { *; }",
     "-dontoptimize",
     "-dontobfuscate",
     proguardKeepLimitedSerializability,
     proguardKeepAllScala,
-    "-keep interface scala.ScalaObject"
-  )
-
-  override def proguardInJars = Path.fromFile("lib_managed/scala_2.8.1.RC2/compile/scala-swing-2.8.1.RC2.jar") +++ Path.fromFile(scalaLibraryJar) +++ super.proguardInJars
+    "-keep class ch.epfl.** { *; }")
 }
+
